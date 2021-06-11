d1faebfe76480f3caf4b43955e66c33827d4a0cb
elisvcodes <sultanoveli@outlook.com>
2021-06-10 20:11:40 -0400

    Added middleware

--- server/app.js
+++ server/app.js
@@ -5,6 +5,11 @@ const app = express();
 
 require('./db/db');
 
+// required Middlewares
 app.use(cors());
+app.use(express.json());
+
+// User Middleware
+app.use('/api/v1/user', require('./routes/user'));
 
 app.listen(7000);
