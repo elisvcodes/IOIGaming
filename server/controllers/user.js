0b0779137256a806ba7b0fece0894886481b03b0
elisvcodes <sultanoveli@outlook.com>
2021-06-11 10:36:08 -0400

    Login Controller and changed signup controller a little

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 
-exports.createUser = (req, res) => {
+exports.signup = (req, res) => {
   const user = new User(req.body);
   user.save((err, result) => {
     if (err) {
@@ -8,5 +8,22 @@ exports.createUser = (req, res) => {
     }
     result.generateToken();
     res.status(200).json(result);
+  });
+};
+
+exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email }).exec(async (err, user) => {
+    if (err) {
+      return res.status(400).json(err);
+    }
+    const correctPassword = await user.checkPassword(password);
+    if (correctPassword) {
+      user.generateToken();
+      res.status(200).json(user);
+    } else {
+      res.status(200).json('Bad Login');
+    }
   });
 };
