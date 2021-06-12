2d50a4586d69672df65321329e29ed185c31c257
elisvcodes <sultanoveli@outlook.com>
2021-06-12 11:01:07 -0400

    added timestamps for users model

--- server/models/user.js
+++ server/models/user.js
@@ -2,17 +2,20 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true },
-  email: { type: String, unique: true, required: true, trim: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' },
-  tokens: [
-    {
-      token: String,
-    },
-  ],
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, unique: true, required: true, trim: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    tokens: [
+      {
+        token: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 userSchema.pre('save', async function (next) {
   if (this.isModified('password')) {
