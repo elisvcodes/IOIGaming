d7304f8de78ece465c8ae86dcee899013218fc46
elisvcodes <sultanoveli@outlook.com>
2021-06-12 11:02:08 -0400

    created a createCategory controller

---
+++ server/controllers/category.js
@@ -0,0 +1,28 @@
+const Category = require('../models/category');
+const slugify = require('slugify');
+
+exports.createCategory = (req, res) => {
+  const data = {
+    owner: req.user._id,
+    name: req.body.name,
+    description: req.body.description,
+    slug: slugify(req.body.name, { replacement: '-', lower: true }),
+  };
+
+  if (req.body.parentId) {
+    data.parentId = req.body.parentId;
+  }
+
+  if (req.file) {
+    data.categoryImg = req.file.filename;
+  }
+
+  const category = new Category(data);
+
+  category.save((err, result) => {
+    if (err) {
+      return res.status(400).json({ msg: err });
+    }
+    res.status(200).json(result);
+  });
+};
