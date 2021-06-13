b1ffbed078a1c9c3ff5a280d4fa42be3101beb72
elisvcodes <sultanoveli@outlook.com>
2021-06-12 22:18:08 -0400

    product controller

---
+++ server/controllers/product.js
@@ -0,0 +1,35 @@
+const Category = require('../models/category');
+const Product = require('../models/product');
+
+exports.createProduct = (req, res) => {
+  console.lo;
+  const data = {
+    owner: req.user._id,
+    name: req.body.name,
+    longDescription: req.body.longDescription,
+    categoryId: req.body.categoryId,
+    price: req.body.price,
+    sku: req.body.sku,
+    qunatity: req.body.quantity,
+  };
+
+  if (req.body.shortDescription) {
+    data.shortDescription = req.body.shortDescription;
+  }
+
+  if (req.files) {
+    data.productImg = req.files.map((img) => {
+      return {
+        img: img.filename,
+      };
+    });
+  }
+
+  const product = new Product(data);
+  product.save((err, result) => {
+    if (err) {
+      return res.status(400).json(err);
+    }
+    res.status(200).json(result);
+  });
+};
