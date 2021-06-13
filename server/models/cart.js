997a69721bdf77dedf0883f224492bbea5256cd2
elisvcodes <sultanoveli@outlook.com>
2021-06-13 15:46:39 -0400

    created a cart model

---
+++ server/models/cart.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  items: [
+    {
+      item: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+      quantity: Number,
+    },
+  ],
+});
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
