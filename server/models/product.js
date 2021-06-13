4a1b5db6804d4de854bcae437c84bc2f99613501
elisvcodes <sultanoveli@outlook.com>
2021-06-12 22:17:50 -0400

    product model

---
+++ server/models/product.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema(
+  {
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    name: { type: String, require: true },
+    shortDescription: { type: String },
+    longDescription: { type: String, require: true },
+    categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+    price: { type: Number, require: true },
+    sku: { type: String, require: true },
+    quantity: { type: Number, require: true },
+    productImg: [
+      {
+        img: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
