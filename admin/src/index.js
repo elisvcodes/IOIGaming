212a7252b38d068edf0dac1b7e266b10eb4b7163
elisvcodes <sultanoveli@outlook.com>
2021-06-15 10:48:58 -0400

    added required items for redux

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import Thunk from 'redux-thunk';
+
+import reducer from './_reducers/index';
+
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(Thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
