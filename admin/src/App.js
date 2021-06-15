0a14aef6eb297465691e8f3ed7f023d0179f46b7
elisvcodes <sultanoveli@outlook.com>
2021-06-15 10:48:41 -0400

    added private routes and a useEffect to check if user logged in

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage/index';
 import Products from './pages/Products/index';
 import Categories from './pages/Categories/index';
+import Login from './pages/Login/index';
+import { useDispatch } from 'react-redux';
+import { isLoggedIn } from './_actions/auth';
+import Privateroute from './components/HOF/Privateroute.js';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(isLoggedIn());
+  });
   return (
     <>
       <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/products" component={Products} />
-        <Route exact path="/categories" component={Categories} />
+        <Privateroute exact path="/" component={Homepage} />
+        <Privateroute path="/categories" component={Categories} />
+        <Privateroute path="/products" component={Products} />
+        <Route path="/login" component={Login} />
       </Switch>
     </>
   );
