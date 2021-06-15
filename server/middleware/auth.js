c07075a598c54e080c95bb355a69c073c632d69b
elisvcodes <sultanoveli@outlook.com>
2021-06-15 09:22:21 -0400

    switched to using cookies instead of headers for auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,11 +2,11 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 exports.protected = (req, res, next) => {
-  if (req.headers.authorization === undefined) {
+  if (req.cookies.user_token_jwt === undefined) {
     return res.status(402).json({ msg: 'Not authorized' });
   }
 
-  const token = req.headers.authorization.replace('Bearer ', '');
+  const token = req.cookies.user_token_jwt;
   const decoded = jwt.verify(token, 'hello');
 
   User.findOne({ _id: decoded._id, 'tokens.token': token }).exec(
