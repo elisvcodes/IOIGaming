2435d0f0f81dedecae9769090523bf0a75fd959b
elisvcodes <sultanoveli@outlook.com>
2021-06-16 17:13:05 -0400

    Logout API

--- admin/src/_api/auth.js
+++ admin/src/_api/auth.js
@@ -4,3 +4,10 @@ export const login = (creds) =>
   axios.post('http://localhost:7000/api/v1/user/login', creds, {
     withCredentials: true,
   });
+
+export const logout = () =>
+  axios.post(
+    'http://localhost:7000/api/v1/user/logout',
+    {},
+    { withCredentials: true }
+  );
