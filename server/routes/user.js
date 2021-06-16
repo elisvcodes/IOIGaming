749712fc98bcf918acc45eeaf6e0521ba471a891
elisvcodes <sultanoveli@outlook.com>
2021-06-16 17:12:30 -0400

    added logout route

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { signup, login } = require('../controllers/user');
+const { signup, login, logout } = require('../controllers/user');
+const { protected } = require('../middleware/auth');
 
 router.post('/signup', signup);
 router.post('/login', login);
-
+router.post('/logout', protected, logout);
 module.exports = router;
