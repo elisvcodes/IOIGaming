7426310e65a8286eae3bf60903958c8792c86239
elisvcodes <sultanoveli@outlook.com>
2021-06-17 12:00:25 -0400

    added a controller that gets a list of categories

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,6 +1,29 @@
 const Category = require('../models/category');
 const slugify = require('slugify');
 
+const displayCategory = (categories, parentId = null) => {
+  let category;
+  let categoriesList = [];
+
+  if (parentId == null) {
+    category = categories.filter((cat) => cat.parentId == undefined);
+  } else {
+    category = categories.filter((cat) => cat.parentId == parentId);
+  }
+
+  for (const cat of category) {
+    categoriesList.push({
+      _id: cat._id,
+      name: cat.name,
+      description: cat.description,
+      slug: cat.slug,
+      parentId: cat.parentId,
+      children: displayCategory(categories, cat._id),
+    });
+  }
+  return categoriesList;
+};
+
 exports.createCategory = (req, res) => {
   const data = {
     owner: req.user._id,
@@ -26,5 +49,15 @@ exports.createCategory = (req, res) => {
       return res.status(400).json({ msg: err });
     }
     res.status(200).json(result);
+  });
+};
+
+exports.getCategories = (req, res) => {
+  Category.find({}).exec((err, result) => {
+    if (err) {
+      return res.status(400).json({ msg: err });
+    }
+    const list = displayCategory(result);
+    res.status(200).json(list);
   });
 };
