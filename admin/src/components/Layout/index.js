2164f104b0967b6dfce2cdf027952fdff08657fe
elisvcodes <sultanoveli@outlook.com>
2021-06-19 12:22:17 -0400

    improved Layout component

--- admin/src/components/Layout/index.js
+++ admin/src/components/Layout/index.js
@@ -1,16 +1,27 @@
-import { Grid } from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
 import React from 'react';
-import Sidebar from './Sidebar';
+import Sidebar from './Sidebar/index';
+import Header from './Header/index';
 
 export default function Layout({ children, sidebar }) {
   return (
     <>
-      <Grid item sm={2}>
-        <Sidebar />
-      </Grid>
-      <Grid item sm={10} style={{ marginLeft: 'auto' }}>
-        {children}
-      </Grid>
+      {sidebar ? (
+        <>
+          <Grid item sm={2}>
+            <Sidebar />
+          </Grid>
+          <Grid item sm={10} style={{ marginLeft: 'auto' }}>
+            <Header />
+            {children}
+          </Grid>
+        </>
+      ) : (
+        <>
+          <Header />
+          <Container style={{ padding: '50px 0' }}>{children}</Container>
+        </>
+      )}
     </>
   );
 }
