cf1ba54a6d4aa444e533f0e8074ddd7b889b4008
elisvcodes <sultanoveli@outlook.com>
2021-06-19 11:18:43 -0400

    updated the status code

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 exports.protected = (req, res, next) => {
   if (req.cookies.user_token_jwt === undefined) {
-    return res.status(402).json({ msg: 'Not authorized' });
+    return res.status(401).json({ msg: 'Not authorized' });
   }
 
   const token = req.cookies.user_token_jwt;
