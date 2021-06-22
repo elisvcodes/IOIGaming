1020c0050f86938193a976ea045ff363e8d85472
elisvcodes <sultanoveli@outlook.com>
2021-06-22 18:21:15 -0400

    Updated Form component to accept new arguments

--- admin/src/components/UI/Form/index.js
+++ admin/src/components/UI/Form/index.js
@@ -1,22 +1,38 @@
-import { Button } from '@material-ui/core';
+import { Button, makeStyles } from '@material-ui/core';
 import React from 'react';
 import Inputs from '../inputs/index';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+      marginRight: theme.spacing(1.1),
+    },
+  },
+}));
+
 export default function Form(props) {
+  const classes = useStyles();
   return (
     <>
-      <form onSubmit={props.onSubmit}>
+      <form onSubmit={props.onSubmit} className={classes.root}>
         {props.fields.map((field, index) => (
           <Inputs
             key={index}
             type={field.type}
             name={field.name}
             label={field.label}
-            variant="outlined"
+            variant={field.variant}
             className={field.className}
             value={field.value}
             onChange={field.onChange}
             fullWidth={field.fullWidth}
             required={field.required}
+            multiline={field.multiline}
+            rows={field.rows}
+            width={field.width}
+            select={field.select}
           />
         ))}
         <Button type="submit" variant="contained">
