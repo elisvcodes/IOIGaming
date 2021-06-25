f25ad3ea6578fada6444e4ac67d1f40d854a6585
elisvcodes <sultanoveli@outlook.com>
2021-06-25 15:59:46 -0400

    added apis for update and delete

--- admin/src/_api/product.js
+++ admin/src/_api/product.js
@@ -7,3 +7,13 @@ export const createProduct = (data) =>
 
 export const getProducts = () =>
   axios.get('http://localhost:7000/api/v1/product/', { withCredentials: true });
+
+export const updateProduct = (data) =>
+  axios.patch('http://localhost:7000/api/v1/product/update', data, {
+    withCredentials: true,
+  });
+
+export const deleteProduct = (id) =>
+  axios.delete(`http://localhost:7000/api/v1/product/${id}`, {
+    withCredentials: true,
+  });
