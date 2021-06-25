c1600f850233d5dfe11eee9411c2d963e155d163
elisvcodes <sultanoveli@outlook.com>
2021-06-25 15:59:10 -0400

    added some extras

--- admin/src/App.js
+++ admin/src/App.js
@@ -7,6 +7,7 @@ import Login from './pages/Login/index';
 import { useDispatch } from 'react-redux';
 import { isLoggedIn } from './_actions/auth';
 import { getCategories } from './_actions/category';
+import { getProducts } from './_actions/product';
 import Privateroute from './components/HOF/Privateroute.js';
 import Cookies from 'js-cookie';
 function App() {
@@ -14,15 +15,16 @@ function App() {
 
   useEffect(() => {
     dispatch(getCategories());
+    dispatch(getProducts());
     dispatch(isLoggedIn());
   }, []);
   return (
     <>
       <Switch>
-        <Privateroute exact path="/" component={Homepage} />
-        <Privateroute path="/categories" component={Categories} />
-        <Privateroute path="/products" component={Products} />
-        <Route path="/login" component={Login} />
+        <Privateroute exact path='/' component={Homepage} />
+        <Privateroute path='/categories' component={Categories} />
+        <Privateroute path='/products' component={Products} />
+        <Route path='/login' component={Login} />
       </Switch>
     </>
   );
