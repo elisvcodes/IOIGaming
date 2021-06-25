eea03e7e03fdad519a19c15a615c54d342656f04
elisvcodes <sultanoveli@outlook.com>
2021-06-25 16:00:02 -0400

    added products to reducer

--- admin/src/_reducers/index.js
+++ admin/src/_reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import categories from './category';
+import products from './product';
 export default combineReducers({
   auth,
   categories,
+  products,
 });
