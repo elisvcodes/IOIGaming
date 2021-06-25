fcc9ffd4ffc895fc9c3f51a0c77864698be72652
elisvcodes <sultanoveli@outlook.com>
2021-06-25 15:59:33 -0400

    created the actions for update and delete

--- admin/src/_actions/product.js
+++ admin/src/_actions/product.js
@@ -1,12 +1,26 @@
 import * as API from '../_api/product';
 
+export const createProduct = (proData) => async (dispatch) => {
+  const { data, status } = await API.createProduct(proData);
+  if (status === 200) {
+    dispatch(getProducts());
+  }
+};
+
 export const getProducts = () => async (dispatch) => {
   const { data } = await API.getProducts();
   dispatch({ type: 'GET_PRODUCTS', payload: data });
 };
 
-export const createProduct = (proData) => async (dispatch) => {
-  const { data, status } = await API.createProduct(proData);
+export const updateProduct = (proData) => async (dispatch) => {
+  const { data, status } = await API.updateProduct(proData);
+  if (status === 200) {
+    dispatch(getProducts());
+  }
+};
+
+export const deleteProduct = (id) => async (dispatch) => {
+  const { data, status } = await API.deleteProduct(id);
   if (status === 200) {
     dispatch(getProducts());
   }
