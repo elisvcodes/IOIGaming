25e149e7cf7e7bddbc6b848285d0c1efaefd2e4a
elisvcodes <sultanoveli@outlook.com>
2021-07-25 15:01:36 -0400

    added routes to deal with custom pages

--- server/routes/page.js
+++ server/routes/page.js
@@ -3,11 +3,16 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const { protected } = require('../middleware/auth');
-const { createPage } = require('../controllers/page');
+const {
+  createPage,
+  getPages,
+  updatePage,
+  deletePage,
+} = require('../controllers/page');
 
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../public/media/categories'));
+    cb(null, path.join(__dirname, '../public/media/pages'));
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
@@ -17,5 +22,7 @@ let storage = multer.diskStorage({
 let upload = multer({ storage: storage });
 
 router.post('/create', protected, upload.single('heroImage'), createPage);
-
+router.get('/', getPages);
+router.patch('/update', protected, upload.single('heroImage'), updatePage);
+router.delete('/:id', protected, deletePage);
 module.exports = router;
