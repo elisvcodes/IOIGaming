d4041267cf40ccd451dda01e8d12cdbaa3bfb94d
elisvcodes <sultanoveli@outlook.com>
2021-07-25 15:03:50 -0400

    added page actions to reducer

--- admin/src/_reducers/index.js
+++ admin/src/_reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import auth from './auth';
 import categories from './category';
 import products from './product';
+import pages from './page';
 export default combineReducers({
   auth,
   categories,
   products,
+  pages,
 });
