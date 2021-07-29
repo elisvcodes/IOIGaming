21805df4e0ee8d7f1ad2475ef9047b8b3045c154
elisvcodes <sultanoveli@outlook.com>
2021-07-28 20:01:27 -0400

    added a few options for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Layout from './components/Layout';
+import { CssBaseline } from '@material-ui/core';
+import { Switch, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import { useDispatch } from 'react-redux';
+import { getCategories } from './_actions/categories';
 
 export default function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCategories());
+  }, []);
+
   return (
     <div>
-      <h1> Hello</h1>
+      <CssBaseline />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+      </Switch>
     </div>
   );
 }
