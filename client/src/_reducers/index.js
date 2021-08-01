acd01644df3ecd2459ecda6c5ad11737f98deaba
elisvcodes <sultanoveli@outlook.com>
2021-08-01 18:24:34 -0400

    reducer index file

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 import page from './page';
 import categories from './categories';
+import products from './products';
+import cart from './cart';
 export default combineReducers({
   page,
   categories,
+  products,
+  cart,
 });
