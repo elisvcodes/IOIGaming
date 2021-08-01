64bb4a04e6edceccdb7a0ab81d32d7999c5a8ec1
elisvcodes <sultanoveli@outlook.com>
2021-08-01 18:26:15 -0400

    rendering cart on every page

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,28 @@ import React, { useEffect } from 'react';
 import Layout from './components/Layout';
 import { CssBaseline } from '@material-ui/core';
 import { Switch, Route } from 'react-router-dom';
-import Homepage from './pages/Homepage';
+import Homepage from './pages/Homepage/index';
+import Categories from './pages/Categories/index';
+import Product from './pages/Product/index';
+import Cart from './pages/Cart/index';
 import { useDispatch } from 'react-redux';
 import { getCategories } from './_actions/categories';
-
+import { getCart } from './_actions/cart';
 export default function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getCategories());
-  }, []);
+    dispatch(getCart());
+  }, [dispatch]);
 
   return (
     <div>
       <CssBaseline />
       <Switch>
         <Route exact path='/' component={Homepage} />
+        <Route path='/cat/:slug' component={Categories} />
+        <Route path='/products/:slug' component={Product} />
+        <Route path='/cart' component={Cart} />
       </Switch>
     </div>
   );
