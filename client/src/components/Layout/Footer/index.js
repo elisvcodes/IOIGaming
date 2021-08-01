20b55b244ca9d08c603560229a5e22eb82567973
elisvcodes <sultanoveli@outlook.com>
2021-08-01 18:23:53 -0400

    added a footer

---
+++ client/src/components/Layout/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+export default function Footer() {
+  return (
+    <>
+      <div className='footer'>
+        <p>Copyright 2021 IoI Gaming</p>
+      </div>
+    </>
+  );
+}
