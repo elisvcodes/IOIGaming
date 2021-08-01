a4226ae47af15b9356f86769585a1a31fad8b1dc
elisvcodes <sultanoveli@outlook.com>
2021-08-01 18:23:30 -0400

    working on the cart

---
+++ client/src/pages/Cart/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../../components/Layout/index';
+import { Container } from '@material-ui/core';
+export default function Cart() {
+  return (
+    <>
+      <Layout>
+        <Container>
+          <h1>Cart </h1>
+        </Container>
+      </Layout>
+    </>
+  );
+}
