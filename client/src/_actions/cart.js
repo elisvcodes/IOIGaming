3d88013172ec599076eb96519126291a749ef893
elisvcodes <sultanoveli@outlook.com>
2021-08-03 18:10:55 -0400

    actions for incrementing and decrementing

--- client/src/_actions/cart.js
+++ client/src/_actions/cart.js
@@ -4,3 +4,10 @@ export const getCart = () => (dispatch) => {
 export const addToCart = (item) => (dispatch) => {
   dispatch({ type: 'ADD_TO_CART', payload: item });
 };
+export const incrementItem = (item, quantity) => (dispatch) => {
+  dispatch({ type: 'INCREMENT', payload: { item, quantity } });
+};
+
+export const decrementItem = (item, quantity) => (dispatch) => {
+  dispatch({ type: 'DECREMENT', payload: { item, quantity } });
+};
