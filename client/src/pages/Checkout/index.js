1e22f1283e3378d4a58634c69309792639000319
elisvcodes <sultanoveli@outlook.com>
2021-08-21 22:43:24 -0400

    started to work on checkout

---
+++ client/src/pages/Checkout/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Layout from '../../components/Layout/index';
+import { Container } from '@material-ui/core';
+
+export default function Checkout() {
+  return (
+    <>
+      <Layout>
+        <Container>
+          <h2> Checkout</h2>
+        </Container>
+      </Layout>
+    </>
+  );
+}
