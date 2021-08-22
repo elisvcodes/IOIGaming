1639fdf873d2f7748d2be186fa8804d6541acb98
elisvcodes <sultanoveli@outlook.com>
2021-08-21 22:42:59 -0400

    removed a duplicate

--- client/src/_reducers/categories.js
+++ client/src/_reducers/categories.js
@@ -2,8 +2,6 @@ export default (catgeories = [], action) => {
   switch (action.type) {
     case 'GET_CATEGORIES':
       return action.payload;
-    case 'GET_RPODUCTS_BY_CAT':
-      return action.payload;
 
     default:
       return catgeories;
