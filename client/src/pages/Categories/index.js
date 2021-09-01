2aa5216b3b3b0dd95622d9dddc6139c51672ec37
elisvcodes <sultanoveli@outlook.com>
2021-08-31 22:08:02 -0400

    made products more reusable

--- client/src/pages/Categories/index.js
+++ client/src/pages/Categories/index.js
@@ -3,7 +3,7 @@ import Layout from '../../components/Layout';
 import { getProductsByCat } from '../../_actions/products';
 import { useDispatch, useSelector } from 'react-redux';
 import { Grid, Link } from '@material-ui/core';
-import './style.css';
+import Products from '../../components/Products';
 export default function Categories(props) {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -13,32 +13,9 @@ export default function Categories(props) {
   return (
     <>
       <Layout sidebar={{ props: products && products }}>
-        <Grid container>
-          {products.length > 0 ? (
-            products.map((product) => {
-              return (
-                <Grid item xs={12} sm={4} key={product._id}>
-                  <div className='productsCard'>
-                    <Link
-                      href={`/products/${product.slug}`}
-                      underline='none'
-                      color='textPrimary'
-                    >
-                      <img
-                        src={`${product.productImg[0].imageUrl}`}
-                        alt={product.name}
-                      />
-                      <p className='productTitle'>{product.name}</p>
-                      <p className='productPrice'>${product.price}</p>
-                    </Link>
-                  </div>
-                </Grid>
-              );
-            })
-          ) : (
-            <p>No products</p>
-          )}
-        </Grid>
+        <div style={{ marginTop: '20px' }}>
+          <Products products={products} />
+        </div>
       </Layout>
     </>
   );
