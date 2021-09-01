7fc8a254a18c255947374036ffd1f1fef486c886
elisvcodes <sultanoveli@outlook.com>
2021-08-31 22:07:10 -0400

    finished of the checkout page

--- client/src/pages/Checkout/index.js
+++ client/src/pages/Checkout/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../../components/Layout/index';
-
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import axios from 'axios';
 import {
@@ -12,28 +11,115 @@ import {
   Typography,
 } from '@material-ui/core';
 import './style.css';
+import config from '../../util/config';
+import { useHistory } from 'react-router-dom';
+
 export default function Checkout(props) {
   const [succeeded, setSucceeded] = useState(false);
-
   const [error, setError] = useState(null);
-
+  const [orderConfimration, setOrderConfirmation] = useState('');
   const [processing, setProcessing] = useState('');
-
   const [disabled, setDisabled] = useState(true);
-
   const [clientSecret, setClientSecret] = useState('');
-
   const stripe = useStripe();
-
   const elements = useElements();
+  const history = useHistory();
 
-  useEffect(() => {
-    const { clientSecret } = axios.post(
-      `https://${process.env.REACT_APP_BACKEND_URL}/api/v1/payments/payment`,
+  const [customerInfo, setCustomerInfo] = useState({
+    firstName: '',
+    lastName: '',
+    address: '',
+    apt: '',
+    zip: '',
+    city: '',
+    state: '',
+    email: '',
+    phoneNumber: '',
+  });
+
+  const [itemsInCart] = useState(props.location.state.cart.items);
+  const customerInfoOnChange = (e) => {
+    const { name, value } = e.target;
+    setCustomerInfo({ ...customerInfo, [name]: value });
+  };
+
+  const cardOnChange = (e) => {
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : '');
+  };
+
+  useEffect(async () => {
+    const { data } = await axios.post(
+      `${config.SERVER_URI}/api/v1/payments/payment`,
       { total: props.location.state.cartTotal }
     );
-    setClientSecret(clientSecret);
-  });
+    setClientSecret(data.clientSecret);
+  }, []);
+
+  const cardStyle = {
+    style: {
+      base: {
+        color: '#32325d',
+        fontFamily: 'Arial, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '18px',
+        '::placeholder': {
+          color: '#32325d',
+        },
+      },
+      invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a',
+      },
+    },
+  };
+
+  const createOrder = async () => {
+    const { data } = await axios.post(
+      `${config.SERVER_URI}/api/v1/orders/create`,
+      {
+        customerInfo,
+        itemsOrdered: itemsInCart,
+        orderTotal: props.location.state.cartTotal,
+      },
+      { withCredentials: true }
+    );
+    history.push({
+      pathname: '/order-confirmation',
+      state: data,
+    });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setProcessing(true);
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: `${customerInfo.firstName} ${customerInfo.lastName}`,
+          email: customerInfo.email,
+          address: {
+            city: customerInfo.city,
+            state: customerInfo.state,
+            line1: customerInfo.address,
+            line2: customerInfo.apt,
+            postal_code: customerInfo.zip,
+          },
+        },
+      },
+    });
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+      createOrder();
+    }
+  };
+
   return (
     <>
       <Layout>
@@ -41,80 +127,122 @@ export default function Checkout(props) {
           <h3>Checkout</h3>
           <Grid container className='checkout'>
             <Grid item xs={12} md={8}>
-              <form action='' className='form'>
+              <form action='' className='form' onSubmit={onSubmit}>
                 <div className='formItems'>
                   <TextField
                     type='text'
+                    name='firstName'
                     label='First Name'
                     variant='outlined'
                     className='formItem'
+                    value={customerInfo.firstName}
+                    onChange={customerInfoOnChange}
                   />
                   <TextField
                     type='text'
+                    name='lastName'
                     label='Last Name'
                     variant='outlined'
                     className='formItem'
+                    value={customerInfo.lastName}
+                    onChange={customerInfoOnChange}
                   />
                 </div>
                 <div className='formItems'>
                   <TextField
                     type='text'
+                    name='address'
                     label='Address'
                     variant='outlined'
                     className='formItem'
+                    value={customerInfo.address}
+                    onChange={customerInfoOnChange}
                   />
                   <TextField
                     type='text'
+                    name='apt'
                     label='Apt, Suite, Floor (optional)'
                     variant='outlined'
                     className='formItem'
+                    value={customerInfo.apt}
+                    onChange={customerInfoOnChange}
                   />
                 </div>
                 <div className='formItems'>
                   <TextField
-                    type='text'
+                    type='number'
+                    name='zip'
                     label='Zip'
                     variant='outlined'
                     className='formItem'
+                    value={customerInfo.zip}
+                    onChange={customerInfoOnChange}
                   />
                   <TextField
                     type='text'
                     label='City'
+                    name='city'
                     variant='outlined'
                     className='formItem'
+                    value={customerInfo.city}
+                    onChange={customerInfoOnChange}
                   />
                   <TextField
                     type='text'
                     label='State'
+                    name='state'
                     variant='outlined'
                     className='formItem'
+                    value={customerInfo.state}
+                    onChange={customerInfoOnChange}
                   />
                 </div>
                 <div className='formItems'>
                   <TextField
-                    type='text'
+                    type='email'
                     label='Email'
+                    name='email'
                     variant='outlined'
                     className='formItem'
+                    value={customerInfo.email}
+                    onChange={customerInfoOnChange}
                   />
                   <TextField
-                    type='text'
+                    type='number'
                     label='Phone Number'
+                    name='phoneNumber'
                     variant='outlined'
                     className='formItem'
+                    value={customerInfo.phoneNumber}
+                    onChange={customerInfoOnChange}
                   />
                 </div>
                 <h3>Payment</h3>
                 <div id='payment-form'></div>
-                <CardElement id='payment-form' />
+                <CardElement
+                  id='payment-form'
+                  options={cardStyle}
+                  onChange={cardOnChange}
+                />
                 <Button
                   variant='contained'
                   color='primary'
                   size='small'
+                  type='submit'
                   style={{ marginTop: '20px', width: '10%' }}
+                  disabled={processing || disabled || succeeded}
                 >
-                  Pay
+                  {processing ? (
+                    <div className='spinner' id='spinner'></div>
+                  ) : (
+                    'Pay'
+                  )}
                 </Button>
+                {error && (
+                  <div className='card-error' role='alert'>
+                    {error}
+                  </div>
+                )}
               </form>
             </Grid>
             <Grid item xs={12} md={4}>
