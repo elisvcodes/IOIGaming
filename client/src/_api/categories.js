bde6ba206de5b8e41555e54a35ce8e5da6bb77e9
elisvcodes <sultanoveli@outlook.com>
2021-08-31 22:09:44 -0400

    api tweaks

--- client/src/_api/categories.js
+++ client/src/_api/categories.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import config from '../util/config';
 
 export const getCategories = () =>
-  axios.get(`https://${process.env.REACT_APP_BACKEND_URL}/api/v1/category`);
+  axios.get(`${config.SERVER_URI}/api/v1/category`);
--- client/src/_api/pages.js
+++ client/src/_api/pages.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import config from '../util/config';
 
 export const getPage = (slug) =>
-  axios.get(`https://${process.env.REACT_APP_BACKEND_URL}/api/v1/page/${slug}`);
+  axios.get(`${config.SERVER_URI}/api/v1/page/${slug}`);
