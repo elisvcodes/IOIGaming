aa27ee46d9562998f9e2e1cd5f4b90fe6ad5975c
elisvcodes <sultanoveli@outlook.com>
2021-08-31 22:06:32 -0400

    minor tweak

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getPage } from '../../_actions/pages';
 import { Container, Grid, Link } from '@material-ui/core';
 import './styles.css';
-console.log(process.env.DOMAIN);
 export default function Homepage() {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -28,7 +27,6 @@ export default function Homepage() {
     return options;
   };
   const itrCategories = flattenArray(categories);
-  console.log(homepage);
   return (
     <Layout>
       <Container>
