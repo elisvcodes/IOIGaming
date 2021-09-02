632435bbf35ba5775ea773bda968ed98b216ffa7
elisvcodes <sultanoveli@outlook.com>
2021-09-02 14:22:38 -0400

    changed how we get products from API on client

--- client/src/_api/products.js
+++ client/src/_api/products.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 import config from '../util/config';
 
-export const getProductsByCat = (slug) =>
-  axios.get(
-    `${config.SERVER_URI}/api/v1/category/${slug}${window.location.search}`
-  );
+export const getProducts = ({ slug, query, priceQuery }) => {
+  console.log(`${slug}${priceQuery}`);
+  if (slug && priceQuery) {
+    return axios.get(
+      `${config.SERVER_URI}/api/v1/product/${slug}${priceQuery}`
+    );
+  } else if (query) {
+    return axios.get(`${config.SERVER_URI}/api/v1/product/search/${query}`);
+  } else if (slug) {
+    return axios.get(`${config.SERVER_URI}/api/v1/product/${slug}`);
+  }
+};
