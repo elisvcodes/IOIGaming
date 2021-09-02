058ef74be12c66489582b8e69ca1bb2861174600
elisvcodes <sultanoveli@outlook.com>
2021-09-02 14:21:17 -0400

    imrpoved our dispatch

--- client/src/pages/Categories/index.js
+++ client/src/pages/Categories/index.js
@@ -1,23 +1,34 @@
 import React, { useEffect } from 'react';
 import Layout from '../../components/Layout';
-import { getProductsByCat } from '../../_actions/products';
+import { getProducts } from '../../_actions/products';
 import { useDispatch, useSelector } from 'react-redux';
 import Products from '../../components/Products';
+import axios from 'axios';
+import config from '../../util/config';
+
 export default function Categories(props) {
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getProductsByCat(props.match.params.slug));
-  }, [dispatch]);
-
+  useEffect(async () => {
+    dispatch(
+      getProducts({
+        slug: props.match.params.slug,
+        priceQuery: props.location.search,
+      })
+    );
+    // const { data } = await axios.get(
+    //   `${config.SERVER_URI}/api/v1/product/products/${props.match.params.slug}`
+    // );
+    // console.log(data);
+    // setProductData(data);
+  }, [dispatch, props.location]);
   const productsState = useSelector((state) => state.productReducer);
   const { products, isFetching } = productsState;
-
   if (isFetching) {
     return 'loading';
   }
   return (
     <>
-      <Layout sidebar={{ props: products }}>
+      <Layout sidebar={{ products: products, props }}>
         <div style={{ marginTop: '20px' }}>
           <Products products={products} />
         </div>
