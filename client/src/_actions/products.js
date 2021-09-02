4256c2636ee3e9de02f0d1365ce308da1acaf1da
elisvcodes <sultanoveli@outlook.com>
2021-09-02 14:22:46 -0400

    minor tweak

--- client/src/_actions/products.js
+++ client/src/_actions/products.js
@@ -1,9 +1,11 @@
 import * as API from '../_api/products';
 
-export const getProductsByCat = (slug) => async (dispatch) => {
-  dispatch({ type: 'GET_RPODUCTS_BY_CAT_REQUEST' });
-  const { data, status } = await API.getProductsByCat(slug);
-  if (status === 200) {
-    dispatch({ type: 'GET_RPODUCTS_BY_CAT', payload: data });
-  }
-};
+export const getProducts =
+  ({ slug, query, priceQuery }) =>
+  async (dispatch) => {
+    dispatch({ type: 'GET_RPODUCTS_REQUEST' });
+    const { data, status } = await API.getProducts({ slug, query, priceQuery });
+    if (status === 200) {
+      dispatch({ type: 'GET_RPODUCTS', payload: data });
+    }
+  };
