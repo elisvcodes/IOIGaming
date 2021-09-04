a2e7e0098e501abe852706707e6220c69bb69546
elisvcodes <sultanoveli@outlook.com>
2021-09-04 17:29:35 -0400

    added the ability to graph order data

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -1,4 +1,30 @@
 const Order = require('../models/order');
+const mongoose = require('mongoose');
+
+const populateEmptyDays = (resultsFromRes) => {
+  let results = resultsFromRes;
+  let hasResult = [];
+  for (let result of results) {
+    hasResult.push(result._id);
+  }
+
+  for (let i = 1; i <= 7; i++) {
+    if (!hasResult.includes(i)) {
+      results.push({ _id: i, orders: 0 });
+    }
+  }
+  return results;
+};
+
+const compare = (a, b) => {
+  if (a._id < b._id) {
+    return -1;
+  }
+  if (a._id > b._id) {
+    return 1;
+  }
+  return 0;
+};
 
 exports.createOrder = (req, res) => {
   const { firstName, lastName, address, apt, zip, city, state, email, phone } =
@@ -27,16 +53,39 @@ exports.createOrder = (req, res) => {
 };
 
 exports.getOrders = (req, res) => {
-  Order.find({}).exec((err, result) => {
-    if (err) {
-      return res.status(400).json({ msg: err });
-    }
-    res.status(200).json(result);
-  });
+  Order.find({})
+    .populate({ path: 'itemsOrdered.item' })
+    .sort({ createdAt: -1 })
+    .exec((err, result) => {
+      if (err) {
+        return res.status(400).json({ msg: err });
+      }
+      res.status(200).json(result);
+    });
 };
 
 exports.updateOrder = (req, res) => {
   Order.findByIdAndUpdate({ _id: req.body._id }, req.body, {
     returnOriginal: false,
+  });
+};
+
+exports.weeklyOrdersGraph = (req, res) => {
+  Order.aggregate([
+    {
+      $group: {
+        _id: {
+          $dayOfWeek: { date: '$createdAt', timezone: 'America/New_York' },
+        },
+        orders: { $sum: 1 },
+      },
+    },
+  ]).exec((err, result) => {
+    if (err) {
+      return res.status(400).json(err);
+    }
+    const resultsWithEmptyDays = populateEmptyDays(result);
+    resultsWithEmptyDays.sort(compare);
+    res.status(200).json(resultsWithEmptyDays);
   });
 };
--- server/routes/order.js
+++ server/routes/order.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder, getOrders, updateOrder } = require('../controllers/order');
+const {
+  createOrder,
+  getOrders,
+  updateOrder,
+  weeklyOrdersGraph,
+} = require('../controllers/order');
 const { protected } = require('../middleware/auth');
 
 router.post('/create', createOrder);
 router.get('/', protected, getOrders);
+router.get('/weeklygraph', protected, weeklyOrdersGraph);
 router.patch('/update', protected, updateOrder);
 module.exports = router;
