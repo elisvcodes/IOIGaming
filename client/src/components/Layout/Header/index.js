8ec4ef6ef420e5cb9a38ff9bcf35a6bc6318ab09
elisvcodes <sultanoveli@outlook.com>
2021-09-04 17:55:42 -0400

    hiding search and cart when user is at checkout

--- client/src/components/Layout/Header/index.js
+++ client/src/components/Layout/Header/index.js
@@ -8,7 +8,7 @@ import {
 } from '@material-ui/core';
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import './style.css';
 import { AiOutlineSearch } from 'react-icons/ai';
@@ -17,6 +17,7 @@ export default function Header() {
   const cart = useSelector((state) => state.cart);
   const [q, setQ] = useState('');
   const history = useHistory();
+  const location = useLocation();
   return (
     <>
       <AppBar position='relative' elevation={0}>
@@ -32,51 +33,59 @@ export default function Header() {
                 IoI
               </Link>
             </Typography>
-            <form
-              action=''
-              onSubmit={(e) => history.push(`/search/?q=${q}`)}
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                width: '70%',
-              }}
-            >
-              <InputBase
-                placeholder='Search…'
-                name='q'
-                value={q}
-                onChange={(e) => setQ(e.target.value)}
-                inputProps={{ 'aria-label': 'search' }}
-                style={{
-                  borderRadius: '1%',
-                  background: 'white',
-                }}
-                startAdornment={
-                  <button
+            {location.pathname === '/checkout' ||
+            location.pathname === '/order-confirmation' ? (
+              ''
+            ) : (
+              <>
+                {' '}
+                <form
+                  action=''
+                  onSubmit={(e) => history.push(`/search/?q=${q}`)}
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '70%',
+                  }}
+                >
+                  <InputBase
+                    placeholder='Search…'
+                    name='q'
+                    value={q}
+                    onChange={(e) => setQ(e.target.value)}
+                    inputProps={{ 'aria-label': 'search' }}
                     style={{
-                      border: 'none',
-                      background: 'none',
-                      cursor: 'pointer',
+                      borderRadius: '1%',
+                      background: 'white',
                     }}
-                    type='submit'
-                  >
-                    <AiOutlineSearch color='black' size={20} />
-                  </button>
-                }
-                fullWidth
-              />
-            </form>
-            <Typography component={'span'}>
-              <div className='cartIcon'>
-                <Link href='/cart' color='inherit'>
-                  <span className='cartItems'>
-                    {cart.items.reduce((acc, cur) => acc + cur.quantity, 0)}{' '}
-                  </span>
-                  <AiOutlineShoppingCart size={25} />
-                </Link>
-              </div>
-            </Typography>
+                    startAdornment={
+                      <button
+                        style={{
+                          border: 'none',
+                          background: 'none',
+                          cursor: 'pointer',
+                        }}
+                        type='submit'
+                      >
+                        <AiOutlineSearch color='black' size={20} />
+                      </button>
+                    }
+                    fullWidth
+                  />
+                </form>
+                <Typography component={'span'}>
+                  <div className='cartIcon'>
+                    <Link href='/cart' color='inherit'>
+                      <span className='cartItems'>
+                        {cart.items.reduce((acc, cur) => acc + cur.quantity, 0)}
+                      </span>
+                      <AiOutlineShoppingCart size={25} />
+                    </Link>
+                  </div>
+                </Typography>
+              </>
+            )}
           </Toolbar>
         </Container>
       </AppBar>
