6cf7fddcf2ce2ea04577c7cd152ccbe143f001be
elisvcodes <sultanoveli@outlook.com>
2021-09-04 18:44:52 -0400

    tweaks

--- admin/src/index.js
+++ admin/src/index.js
@@ -9,13 +9,7 @@ import thunk from 'redux-thunk';
 
 import reducer from './_reducers/index';
 
-const store = createStore(
-  reducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(reducer, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
--- client/src/index.js
+++ client/src/index.js
@@ -8,13 +8,7 @@ import reducer from './_reducers/index';
 import thunk from 'redux-thunk';
 
 import './global.css';
-const store = createStore(
-  reducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(reducer, compose(applyMiddleware(thunk)));
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
