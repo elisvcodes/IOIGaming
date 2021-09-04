a901e838c87018eee59b206a41f92aaec0c8ecf1
elisvcodes <sultanoveli@outlook.com>
2021-09-04 18:12:39 -0400

    tweak

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  'mongodb+srv://user:123@cluster0.i8l45.mongodb.net/zebraShop?retryWrites=true&w=majority',
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i8l45.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
